a55e43765ced6f4669af4ebd2408f74bb3651cde
maazkhan75 <maazkhan75555@gmail.com>
2024-04-20 23:56:30 +0500

    signup and login bugs functionality improved

--- SQL/SQL_ProceduresForRailSync.sql
+++ SQL/SQL_ProceduresForRailSync.sql
@@ -1,41 +1,59 @@
-SELECT * FROM [User]
-GO
 
 --------PROCEDURE FOR SIGNUP FUNCTIONALITY-------
-CREATE PROCEDURE createUser 
+CREATE PROCEDURE CreateUser 
 	@UserName nvarchar(255),
-	@Password nvarchar(255)
+	@CNIC nvarchar(255),
+	@Password nvarchar(255),
+	@PhoneNo nvarchar(255)
 AS 
 BEGIN	
 	SET NOCOUNT ON;
 
-	-- Check if the username already exists
-	IF EXISTS (SELECT 1 FROM [User] WHERE UserName=@UserName)
+	-- Check if the user already exists
+	IF EXISTS (SELECT 1 FROM [User] WHERE CNIC=@CNIC)
 	BEGIN
-		-- Username already exists, return an error message
-		RAISERROR ('UserName already exists.', 16, 1);
+		-- User already exists, return an error message
+		RAISERROR ('User already exists!', 16, 1);
 		RETURN -1;
 	END
 
 	-- Insert the new user into the User table
-	INSERT INTO [User] ([Id], [UserName], [Password])
-	VALUES(NEWID(), @UserName, @Password);
+	INSERT INTO [User] ([Id], [UserName], [Password],[CNIC],[PhoneNo])
+	VALUES(NEWID(), @UserName, @Password,@CNIC,@PhoneNo);
 
 	-- Return success message
 	SELECT 'User created successfully!' AS message;
-
 END
 
+--testing--
+CreateUser 'maazkhan75','3520297089087','password1965','03204553255'
+
+drop proc CreateUser
+
+INSERT INTO [User] ([Id], [UserName], [Password],[CNIC],[PhoneNo])
+VALUES(NEWID(), awais, awais123,1352049128298,03239292982);
+
+delete from [User]
+where UserName='user17'
+SELECT * FROM [User]
+GO
+
+truncate table [User]  --we have to remove one by one 
+
+
+--******************************************************************************************************************************************--
+
 --------PROCEDURE FOR LOGIN FUNCTIONALITY-------
-CREATE PROCEDURE authenticateUser 
-	@UserName nvarchar(255),
+
+CREATE PROCEDURE AuthenticateUser 
+	@CNIC nvarchar(255),
 	@Password nvarchar(255)
 AS 
 BEGIN	
 	SET NOCOUNT ON;
 
-	-- Check if the username and password combination exists
-	IF EXISTS (SELECT 1 FROM [User] WHERE  UserName = @UserName AND  Password = @Password)
+	-- Check if the CNIC and password combination exists
+	IF EXISTS (SELECT 1 FROM [User] WHERE  CNIC = @CNIC AND  Password = @Password)
 	BEGIN
 		-- User account found, return a success message
 		SELECT 'User account found!' AS message;
@@ -49,3 +67,5 @@ BEGIN
 
 END
 
+--testing
+drop proc AuthenticateUser
--- routes/signup.js
+++ routes/signup.js
@@ -5,13 +5,15 @@ const sql = require('mssql');
 // Route handler for user signup
 module.exports = (pool) => {
   router.post('/', async (req, res) => {
-    const { username, password } = req.body;
+    const { username, cnic, password, phoneNo } = req.body;
 
     try {
       // Call the stored procedure to create a new user
       const result = await pool.request()
         .input('UserName', sql.NVarChar(255), username)
+        .input('CNIC', sql.NVarChar(255), cnic)
         .input('Password', sql.NVarChar(255), password)
+        .input('PhoneNo', sql.NVarChar(255), phoneNo)
         .execute('CreateUser');
 
       if (result.returnValue === 0) {
@@ -22,7 +24,7 @@ module.exports = (pool) => {
     } catch (error) {
 		console.error('Error creating user:', error);  //for displaying on terminal ( the actual error )
 		// Send the error message to the client side for display
-		res.render('signup', { errorMessage: 'UserName already exits! Please input different User Name.' });
+		res.render('signup', { errorMessage: 'User already exits!' });
     }
   });
 
