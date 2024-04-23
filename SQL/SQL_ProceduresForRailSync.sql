b48af8e048ce751c532be305692d9eba7eed4405
maazkhan75 <maazkhan75555@gmail.com>
2024-04-23 23:35:45 +0500

    improvements in database and written some procedures

--- SQL/SQL_ProceduresForRailSync.sql
+++ SQL/SQL_ProceduresForRailSync.sql
@@ -49,6 +49,82 @@ BEGIN
 
 END
 
+--------------------------------------------------------------------------------------------------------
+
+
+--------PROCEDURE FOR SHOW PROFILE FUNCTIONALITY-------
+
+CREATE PROCEDURE ShowUser 
+	@CNIC nvarchar(255)
+AS 
+BEGIN	
+	SET NOCOUNT ON;
+
+	 SELECT * FROM [User] WHERE CNIC = @CNIC;
+END
+
+
+--------PROCEDURE FOR UPDATE PROFILE FUNCTIONALITY-------
+CREATE PROCEDURE UpdateUser 
+	@CNIC nvarchar(255),
+	@UserName nvarchar(255),
+	@Password nvarchar(255),
+	@PhoneNo nvarchar(255)
+AS 
+BEGIN	
+	SET NOCOUNT ON;
+
+		 UPDATE [User]
+		 SET UserName=@UserName,[Password]=@Password,PhoneNo=@PhoneNo
+		 WHERE CNIC=@CNIC
+END
+
+--------PROCEDURE FOR SHOWING BOOKED TICKETS OF USER-------
+CREATE PROCEDURE ShowBookedTickets
+	@UserId nvarchar(255)
+AS 
+BEGIN	
+	SET NOCOUNT ON;
+
+	IF EXISTS (SELECT 1 FROM [Ticket] WHERE  UserId = @UserId)
+	BEGIN
+		SELECT * FROM [Ticket] WHERE  UserId = @UserId
+	END
+	ELSE
+	BEGIN
+		-- Return error message if no ticket of user found
+		RAISERROR ('no ticket of user found!', 16, 1);
+		RETURN -1;
+	END
+END
+
+
+
+--------PROCEDURE FOR CANCEL TICKET OF USER-------
+CREATE PROCEDURE CancelTicket
+	@UserId nvarchar(255),
+AS 
+BEGIN	
+	SET NOCOUNT ON;
+
+	IF EXISTS (SELECT 1 FROM [Ticket] WHERE  UserId = @UserId)
+	BEGIN
+	
+		DELETE FROM [Ticket] WHERE  UserId = @UserId
+	END
+	ELSE
+	BEGIN
+		-- Return error message if no ticket of user found
+		RAISERROR ('no ticket of user found!', 16, 1);
+		RETURN -1;
+	END
+END
+
+
+
+
+
+
 
 
 
@@ -69,3 +145,30 @@ GO
 truncate table [User]  --we have to remove one by one 
 
 drop proc AuthenticateUser
+
+
+exec ShowUser '3520297089087'
+
+UpdateUser '3520297089087','m maaz khan','mypass123','03204553255'
+
+INSERT INTO [Ticket] ([TicketId], [UserId],[SeatNo],[StartingStation],[EndingStation],[date])
+VALUES(NEWID(),'0CD8799B-6898-4682-8C22-75A6C7224DA0',1,'KHI','LHR','2025-3-13')
+
+SELECT * FROM [Ticket]
+
+ShowBookedTickets '0CD8799B-6898-4682-8C22-75A6C7224DA0'
+
+update  [Ticket]
+set date_and_time='2024-2-1 10:13:02'
+where TicketId='F8553E69-5A65-4EA3-AFFE-A7C8CEB704AA'
+
+update  [Ticket]
+set Date_and_Time='2025-2-1 20:03:12'
+where TicketId='8FDD6A4D-938B-4871-88AA-DC1F7B0D8A1D'
+
+
+alter table [Ticket] drop  column date
+alter table [ticket] add  Date_and_Time DATETIME
+
+
+
