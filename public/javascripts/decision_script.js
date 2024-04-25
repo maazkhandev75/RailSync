9a6c0f02ea747c04f7f81872c9fd526927ec8896
maazkhan75 <maazkhan75555@gmail.com>
2024-04-25 21:32:41 +0500

    session is working like a charm and also redirecting to userDashBoard

--- public/javascripts/decision_script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('signup-btn').addEventListener('click', function() {
-	window.location.href = '/signupForm';
-  });
-  
-  document.getElementById('login-btn').addEventListener('click', function() {
-	window.location.href = '/loginForm';
-  });
-  
