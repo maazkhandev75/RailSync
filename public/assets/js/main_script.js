1313439bd8998e557c9fc923e0699edb67ea03e0
maazkhan75 <maazkhan75555@gmail.com>
2024-07-10 23:51:22 +0500

    loader

--- public/assets/js/main_script.js
+++ public/assets/js/main_script.js
@@ -1,15 +1,15 @@
 
 // Simulate a delay of 2 seconds (for mimicring the loading of elements when goes online)  //code to execute after the delay(basically hiding the loader after 2s)
-// setTimeout(function() {
-//     document.getElementById("preloader").style.display = 'none';  // hide prelaoder 
-//     document.getElementById("content").style.opacity = '1'; // Fade in content
-// }, 2000);
+setTimeout(function() {
+    document.getElementById("preloader").style.display = 'none';  // hide prelaoder 
+    document.getElementById("content").style.opacity = '1'; // Fade in content
+}, 2000);
 
 //JS for preloder hiding on load of all elements. 
 //(the orignal functionality when we host the website on remote server instead of local server)
-window.onload = function()
-{
-document.getElementById("preloader").style.display='none';
-document.getElementById("content").style.opacity = '1'; // Fade in content
+// window.onload = function()
+// {
+// document.getElementById("preloader").style.display='none';
+// document.getElementById("content").style.opacity = '1'; // Fade in content
 
-};
+// };
